3c13cce798eba8cf72a494e5f2edddadc52e0768
Be-rs <betinarohenkohlschmitz@gmail.com>
2026-01-08 09:50:29 -0300

    verificador de idade

--- aula012ex/ex.013/modelo/script.js
+++ aula012ex/ex.013/modelo/script.js
@@ -3,6 +3,8 @@ var data = new Data()
 var ano = data.getFullYear()
 var fano = document.getElementById('textano')
 var res = document.getElementById('res')
+
+
 if( fano.value.length == 0 || Number(fano.value) > ano ){
     window.alert('Algo esta errado tente novamente')
 }else{
